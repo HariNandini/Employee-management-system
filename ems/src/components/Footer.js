ac290d0c5d77c659730bfbf3a85184cedbc5bdf0
HariNandini <84557403+HariNandini@users.noreply.github.com>
2024-02-26 13:32:33 +0530

    Add files via upload

---
+++ ems/src/components/Footer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         
+      }
+    }
+    
+
+  render() {
+    return (
+      <div>
+        <footer className='footer'>
+            <span className='text-muted'>Copyrights reserved to me. </span>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default Footer
