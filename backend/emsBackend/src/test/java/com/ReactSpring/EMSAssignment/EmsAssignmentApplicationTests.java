c819cfcc1ea464e5eae332421dbb85c9c83c7456
HariNandini <84557403+HariNandini@users.noreply.github.com>
2024-02-26 13:52:47 +0530

    Add files via upload

---
+++ backend/emsBackend/src/test/java/com/ReactSpring/EMSAssignment/EmsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReactSpring.EMSAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
